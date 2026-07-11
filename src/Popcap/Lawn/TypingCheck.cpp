8abedac0c0140ade00d0bf7d52c8ec37a8e748c0
bluisblu <53455507+bluisblu@users.noreply.github.com>
2026-07-11 03:39:06 -0400

    TypingCheck.h/.cpp

--- src/Popcap/Lawn/TypingCheck.cpp
+++ src/Popcap/Lawn/TypingCheck.cpp
@@ -1,1 +1,53 @@
 #include "TypingCheck.h"
+
+TypingCheck::TypingCheck() : mPhrase(), mRecentTyping()
+{
+}
+
+void TypingCheck::AddKeyCode(Sexy::KeyCode theCode)
+{
+    mPhrase.append(1, theCode);
+}
+
+void TypingCheck::AddChar(char theChar)
+{
+    std::string strKey(1, (char)tolower(theChar));
+
+    char codeAsChar = Sexy::GetKeyCodeFromName(strKey);
+
+    mPhrase.append(1, codeAsChar);
+}
+
+void TypingCheck::SetPhrase(const std::string &thePhrase)
+{
+    for (int i = 0; i < (int)thePhrase.length(); ++i)
+    {
+        AddChar(thePhrase[i]);
+    }
+}
+
+bool TypingCheck::Check(Sexy::KeyCode theCode)
+{
+    mRecentTyping.append(1, (char)theCode);
+
+    if (!mPhrase.empty())
+    {
+        if (mRecentTyping.length() > mPhrase.length())
+        {
+            mRecentTyping = mRecentTyping.substr(1, mPhrase.length());
+        }
+
+        if (mRecentTyping == mPhrase)
+        {
+            mRecentTyping.clear();
+            return true;
+        }
+    }
+
+    return false;
+}
+
+TypingCheck::TypingCheck(const std::string &thePhrase) : mPhrase(), mRecentTyping()
+{
+    SetPhrase(thePhrase);
+}
--- src/Popcap/Lawn/TypingCheck.h
+++ src/Popcap/Lawn/TypingCheck.h
@@ -1,6 +1,27 @@
-#pragma once
-
 #ifndef __TYPINGCHECK_H__
 #define __TYPINGCHECK_H__
+
+#include "SexyAppFramework/KeyCodes.h"
+#include <string>
+
+class TypingCheck
+{ /* Size=0x38 */
+  protected:
+    /* 0x0000 */ std::string mPhrase;
+    /* 0x001c */ std::string mRecentTyping;
+
+  public:
+    TypingCheck(const TypingCheck &);
+    TypingCheck(const std::string &);
+    TypingCheck();
+
+    void SetPhrase(const std::string &);
+    void AddKeyCode(Sexy::KeyCode);
+    void AddChar(char);
+    bool Check(char);
+    bool Check(Sexy::KeyCode);
+    ~TypingCheck();
+    TypingCheck &operator=(const TypingCheck &);
+};
 
 #endif
