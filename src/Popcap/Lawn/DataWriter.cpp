d04bc364dfe0508a20aea091796e366b6555cf47
bluisblu <53455507+bluisblu@users.noreply.github.com>
2026-07-11 12:18:20 -0400

    DataWriter.h/.cpp

--- src/Popcap/Lawn/DataWriter.cpp
+++ src/Popcap/Lawn/DataWriter.cpp
@@ -1,1 +1,108 @@
 #include "DataWriter.h"
+
+DataWriter::DataWriter()
+{
+    mFile     = NULL;
+    mData     = NULL;
+    mDataLen  = 0;
+    mCapacity = 0;
+}
+
+DataWriter::~DataWriter()
+{
+    Close();
+}
+
+void DataWriter::Close()
+{
+    if (mFile)
+    {
+        fclose(mFile);
+        mFile = NULL;
+    }
+
+    delete[] mData;
+    mData     = NULL;
+    mDataLen  = 0;
+    mCapacity = 0;
+}
+
+void DataWriter::OpenMemory(unsigned long initialCapacity)
+{
+    Close();
+
+    unsigned long capacity = initialCapacity;
+    if (capacity < 0x20)
+    {
+        capacity = 0x20;
+    }
+
+    mData     = new char[capacity];
+    mCapacity = capacity;
+}
+
+void DataWriter::EnsureCapacity(unsigned long theNumBytes)
+{
+    if (mCapacity < theNumBytes)
+    {
+        while (mCapacity < theNumBytes)
+        {
+            mCapacity <<= 1;
+        }
+
+        char *newData = new char[mCapacity];
+
+        memcpy(newData, mData, mDataLen);
+
+        delete mData;
+        mData = newData;
+    }
+}
+
+void DataWriter::WriteBytes(const void *buffer, unsigned long size)
+{
+    if (mData)
+    {
+        EnsureCapacity(mDataLen + size);
+        memcpy(mData + mDataLen, buffer, size);
+        mDataLen += size;
+    }
+    else if (mFile)
+    {
+        fwrite(buffer, 1, size, mFile);
+    }
+}
+
+void DataWriter::WriteLong(unsigned long value)
+{
+    if (mData)
+    {
+        EnsureCapacity(mDataLen + 4);
+        *(unsigned int *)(mData + mDataLen) = value;
+        mDataLen += 4;
+    }
+    else if (mFile)
+    {
+        fwrite(&value, 1, 4, mFile);
+    }
+}
+
+void DataWriter::WriteShort(unsigned short value)
+{
+    if (mData)
+    {
+        EnsureCapacity(mDataLen + 2);
+        *(unsigned short *)(mData + mDataLen) = value;
+        mDataLen += 2;
+    }
+    else if (mFile)
+    {
+        fwrite(&value, 1, 2, mFile);
+    }
+}
+
+void DataWriter::WriteString(const std::string &value)
+{
+    WriteShort((unsigned short)value.size());
+    WriteBytes(value.data(), (unsigned long)value.size());
+}
--- src/Popcap/Lawn/DataWriter.h
+++ src/Popcap/Lawn/DataWriter.h
@@ -1,6 +1,48 @@
-#pragma once
-
 #ifndef __DATAWRITER_H__
 #define __DATAWRITER_H__
 
-#endif
+#include <string>
+
+// MSVC CRT type
+struct _iobuf;
+
+class DataWriter
+{
+  public:
+    DataWriter();
+    DataWriter(const DataWriter &other);
+    virtual ~DataWriter();
+
+    DataWriter &operator=(const DataWriter &other);
+
+    bool OpenFile(const std::string &filename);
+    void OpenMemory(unsigned long initialCapacity);
+    void Close();
+    bool WriteToFile(const std::string &filename);
+    void WriteBytes(const void *buffer, unsigned long size);
+    void WriteLong(unsigned long value);
+    void WriteShort(unsigned short value);
+    void WriteByte(unsigned char value);
+    void WriteBool(bool value);
+    void WriteFloat(float value);
+    void WriteDouble(double value);
+    void WriteString(const std::string &value);
+    unsigned int GetPos();
+    void SetLong(unsigned int value, unsigned int offset);
+    void SetShort(unsigned short value, unsigned int offset);
+    void SetByte(unsigned char value, unsigned int offset);
+    char *GetDataPtr();
+    unsigned int GetDataLen();
+
+  protected:
+    void EnsureCapacity(unsigned long theNumBytes);
+
+  protected:
+    /* 0x0000 */ // vfptr
+    /* 0x0004 */ _iobuf *mFile;
+    /* 0x0008 */ char *mData;
+    /* 0x000c */ unsigned int mDataLen;
+    /* 0x0010 */ unsigned int mCapacity;
+};
+
+#endif // __DATAWRITER_H__
