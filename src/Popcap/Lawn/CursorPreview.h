5b7e66b495548534adfb48944c1241cb633f9235
bluisblu <53455507+bluisblu@users.noreply.github.com>
2026-07-12 01:29:47 -0400

    CursorPreview.h/.cpp

--- src/Popcap/Lawn/CursorPreview.h
+++ src/Popcap/Lawn/CursorPreview.h
@@ -1,6 +1,20 @@
-#pragma once
-
 #ifndef __CURSORPREVIEW_H__
 #define __CURSORPREVIEW_H__
+
+#include "GameObject.h"
+
+class CursorPreview : public GameObject
+{   /* Size=0x2c */
+    /* 0x0000: fields for GameObject */
+  public:
+    /* 0x0024 */ int mGridX;
+    /* 0x0028 */ int mGridY;
+
+  public:
+    CursorPreview();
+
+    void Update();
+    void Draw(Sexy::Graphics *);
+};
 
 #endif
